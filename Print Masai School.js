becdc7a6aea5a5928afdca21e50d822671a59fbd
shagunsharma11 <shagun.s@xcitech.in>
2023-09-29 21:28:34 +0530

    Print Masai School

---
+++ Print Masai School.js
@@ -0,0 +1,33 @@
+/*
+Problem Statement: Given a programming task, write a JavaScript program to accomplish the following:
+
+- Print the string "Masai School" on one line.
+- Print the string "A Transformation in Education" on a new line.
+
+Input:
+
+There is no input required for this program.
+
+Output:
+
+Print the following lines:
+
+Masai School
+A Transformation in Education
+
+Example:
+
+Sample Input 1:
+
+No Input
+
+Sample Output 1:
+
+Masai School
+A Transformation in Education
+
+*/
+
+
+console.log("Masai School");
+console.log("A Transformation in Education");
