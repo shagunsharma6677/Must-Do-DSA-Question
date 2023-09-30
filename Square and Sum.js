27e56f382f12d3fc71903261442eeebbd49d8052
shagunsharma11 <shagun.s@xcitech.in>
2023-09-30 10:52:22 +0530

    Square and Sum

---
+++ Square and Sum.js
@@ -0,0 +1,14 @@
+//You are given three numbers, stored in variables with the following names
+//one, two, three
+//You have to find the square of the three numbers, and find the sum of their square values
+//Note : Square of a number, is a number multiplied by itself. For example, the square of 3, is 3 * 3 = 9
+
+//Sample Input 1 
+//1 2 3
+//Sample Output 1
+//14
+
+function squareAndSum(one, two, three) {
+  // Write code here
+  console.log((one*one) + (two*two) + (three*three));
+}
