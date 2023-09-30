ebbd8baf1937ee8ce3aef9be69bc0b67b3ee135f
shagunsharma11 <shagun.s@xcitech.in>
2023-09-30 10:49:01 +0530

    Product of six

---
+++ Product of Six.js
@@ -0,0 +1,13 @@
+//You are given six numbers, stored in variables with the following names
+//one, two, three, four, five, six
+//Print the product of all the six numbers
+
+//Sample Input 1 
+//1 2 3 4 5 6
+//Sample Output 1
+//720
+
+function productOfSix(one, two, three, four, five, six) {
+    // Write code here
+    console.log(one*two*three*four*five*six);
+}
