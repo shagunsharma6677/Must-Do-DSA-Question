0087d2e5597090103e5c966bf11891fe1ece2727
shagunsharma11 <shagun.s@xcitech.in>
2023-09-30 10:46:11 +0530

    Multiply by 50

---
+++ Multiply by 50.js
@@ -0,0 +1,9 @@
+//You are given a number stored in a variable, with the namenumber. 
+//Multiply the number stored in the variablenumberby 50, and print the result obtained.
+//Sample Input 1 --> 2
+//Sample Output 1 --> 100
+
+function multiplyBy50(number) {
+    // Write code here
+    console.log(number*50);
+}
