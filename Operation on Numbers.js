4f4a861a86ccd20d47d61c14a6040169f787d8f5
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-10-01 12:13:26 +0530

    Operation on number

---
+++ Operation on Numbers.js
@@ -0,0 +1,13 @@
+//Description - You are given a number, stored in a variable with the namenumber. Perform the following operations on the value stored in the number
+//1. Multiply the value by 3
+//2. Add 7 after it
+//3. Subtract 10 from it
+//After performing all the above operations, print the updated value
+
+//Sample Input 1  --> 4
+//Sample Output 1 --> 9
+
+function operationOnNumbers(number) {
+  // Write code here
+  console.log(number * 3 + 7 - 10);
+}
