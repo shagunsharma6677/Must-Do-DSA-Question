500d38c1ff1c348c67593a764f79245b97d47bed
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-10-01 12:10:04 +0530

    Find x

---
+++ Find X.js
@@ -0,0 +1,16 @@
+//Description
+//You are given a number X. Find the value of X, if
+//1. X is multiplied by 4
+//2. 10 is added to the new value of X.
+
+//Sample Input 1
+//3
+//Sample Output 1
+//19
+
+function findX(x) {
+  // Write code here
+  let a = x * 3;
+  let b = a + 10;
+  console.log(b);
+}
