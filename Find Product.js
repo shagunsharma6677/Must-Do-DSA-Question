14abe459ecb48ca5257e5063eb80100f65469e32
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-10-01 14:29:03 +0530

    Find Product

---
+++ Find Product.js
@@ -0,0 +1,8 @@
+//Description - You are given five numbers stored in variables with the following names
+//one, two, three, four, five
+//Find the product of the five values, and print it
+
+function findProduct(one, two, three, four, five) {
+  // Write code here
+  console.log(one * two * three * four * five);
+}
