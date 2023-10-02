8a170798a80ddb40f629b54d890421f479649090
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-10-02 12:13:24 +0530

    Compare Two Number

---
+++ Compare Two Number.js
@@ -0,0 +1,20 @@
+//Description - You are given two numbers stored in two variables, with the name,num1andnum2
+//Print the result of the following operations
+
+//1. num1 > num2 -> this operation prints true, if num1 is greater than num2, otherwise it prints false
+//2. num1 < num2 -> this operations prints true, if num1 is smaller than num2, otherwise it prints false
+//3. num1 == num2 -> this operation print true, if num1 is equal to num2, otherwise it prints false
+//You have to print the result of three operations in the order as shown above
+
+// Sample Input 1 
+// 4 16 
+// Sample Output 1
+// false
+// true
+
+function compareTwoNumbers(num1, num2) {
+  // Write code here
+  console.log(num1>num2); 
+  console.log(num1<num2);
+  console.log(num1==num2);
+}
