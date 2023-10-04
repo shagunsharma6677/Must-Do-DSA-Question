67e550c5afebb2e665f7c163ee461ef5bb872642
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-10-04 20:19:47 +0530

    Masai even odd

---
+++ Masai Even or Odd.js
@@ -0,0 +1,22 @@
+// Description
+
+// You are given a number, stored in the variable with the nameN
+
+// If the number is even, printEven, else printOdd
+
+// If the number is divisible by 2, the number is consideredEven, else the number is consideredOdd
+
+// Sample Input 1 
+// 5
+// Sample Output 1
+// Odd
+
+function masaiEvenOrOdd(N) {
+    // Write code here
+    
+    if (N%2==0){
+        console.log("Even");
+    } else if (N%2!=0){
+        console.log("Odd");
+    }
+}
