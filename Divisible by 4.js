bf6dbb3dae8b9cfad574ceedb3c8ef97ddf57aaa
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-10-04 20:09:19 +0530

    division by 4

---
+++ Divisible by 4.js
@@ -0,0 +1,23 @@
+// Description
+
+// You are given a number, stored in the variable with the nameN
+
+// PrintYes, if the number is divisible by 4, else printNo
+
+// Note : A number is divisible by 4, if the result of the following expression number % 4 == 0
+
+// Sample Input 1 
+// 12
+// Sample Output 1
+// Yes
+
+function divisibleBy4(N) {
+    // Write code here
+    
+    if(N%4==0){
+        console.log("Yes");
+    } else {
+        console.log("No");
+    }
+
+}
