998533763cedca7b246a5e1f3451814f21715f98
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-10-05 21:25:28 +0530

    Print Grade

---
+++ Print Grade.js
@@ -0,0 +1,30 @@
+// Description
+
+// You are given a number, stored in the variable with the nametotal
+
+// If the following expression is true
+
+// total == 100, print "A", without quotes
+
+// Else if the following expression is true,
+
+// total >= 90, print "B", without quotes
+
+// Else if the following expression is true,
+
+// total >= 80, print "C", without quotes
+
+// Else, print "F", without quotes
+
+function printGrade(total) {
+  // Write code here
+  if (total == 100) {
+    console.log("A");
+  } else if (total >= 90) {
+    console.log("B");
+  } else if (total >= 80) {
+    console.log("C");
+  } else {
+    console.log("F");
+  }
+}
