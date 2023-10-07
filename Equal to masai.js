77de3ffabed83590cca3b6e54f94589c90e2b5b0
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-10-07 16:01:03 +0530

    Equal to masai

---
+++ Equal to masai.js
@@ -0,0 +1,18 @@
+// Description
+// You are given a string, stored in the variable with the namestr
+// PrintYes, if the string is equal to "masai", else printNo
+
+// Sample Input 1
+// man
+// Sample Output 1
+// No
+
+function equaltoMasai(str) {
+  //write code here
+
+  if (str === "masai") {
+    console.log("Yes");
+  } else {
+    console.log("No");
+  }
+}
