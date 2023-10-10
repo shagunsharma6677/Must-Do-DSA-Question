88d831c03d622236bfbf6676bea1d2895ca53bd0
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-10-10 19:35:45 +0530

    Loop 1 to 5

---
+++ Loop 1 to 5.js
@@ -0,0 +1,23 @@
+// Description
+// You have to print all the numbers from 1 to5
+// Print each number on a new line
+// Input
+// No Input
+// Output
+// Print all the numbers from 1 to5, on a new line
+// Sample Input 1
+// No Input
+// Sample Output 1
+// 1
+// 2
+// 3
+// 4
+// 5
+
+function loop1to5() {
+  // Write code here
+
+  for (let i = 1; i <= 5; i++) {
+    console.log(i);
+  }
+}
