1cf45ec8e9e24b860d602c7b4c05777b765bfd50
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-10-16 21:04:21 +0530

    Seven Numbers

---
+++ Seven Numbers.js
@@ -0,0 +1,17 @@
+// Description
+// You are given 7 numbers A, B, C, D, E, F, G. Find out the product of (A + B + C) and (D + E + F + G).
+// Input
+// Thefirst and the only line of the input contains A, B, C, D, E, F, and G.
+// Constraints
+// 1 <= A, B, C, D, E, F, G <= 30
+// Output
+// Print the value of the expression (A + B + C) * (D + E + F + G)
+// Sample Input 1
+// 1 2 3 4 5 6 7
+// Sample Output 1
+// 132
+
+function sevenNumber(A, B, C, D, E, F, G) {
+  // Write code here
+  console.log((A + B + C) * (D + E + F + G));
+}
