d7a1a28c4bc9410ba00afa92e2f025b6c9f419a6
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-10-17 19:21:04 +0530

    The Senior citizens

---
+++ The Senior Citizen.js
@@ -0,0 +1,25 @@
+// Description
+// You are given a number stored in a variable, with the nameage
+// Check whether the age is greater than and equal to 60 or not.
+// If true, Then print "Senior Citizen"
+// otherwise "Not Senior Citizen".
+// Input
+// The first and the only line of the input contains the number stored in the variablenumber
+// Constraints
+// 1 <= N <= 1
+// Output
+// If the value stored inage, is greater than and equal to 60 Then printSenior Citizen, else printNot Senior Citizen
+// Sample Input 1
+// 50
+// Sample Output 1
+// Not Senior Citizen
+
+function isSeniorCitizen(age) {
+  // Write code here
+
+  if (age >= 60) {
+    console.log("Senior Citizen");
+  } else {
+    console.log("Not Senior Citizen");
+  }
+}
