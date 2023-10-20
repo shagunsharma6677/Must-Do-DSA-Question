45f6729de180d32a513751a10bd25c3ca453537b
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-10-20 20:24:53 +0530

    Sum Two Equation

---
+++ Sum Two Equations.js
@@ -0,0 +1,23 @@
+// Description
+// You are given two numbers, stored in a variable with the names,a, b
+// You have to find the sum ofXandY, such that
+// 1. X = (a*5) + (b*3)
+// 2. Y = (a*7) + (b*4)
+// Find the value of sum, such thatsum = X + Y
+// Input
+// The first and the only line of the input contains the value stored in thea, b
+// Output
+// Print the value of the sum, as explained in the problem statement
+// Sample Input 1 
+// 2 3
+// Sample Output 1
+// 45
+
+function sumTwoEqs(a, b) {
+  // write code here
+  
+  let X = (a*5) + (b*3);
+  let Y = (a*7) + (b*4);
+ 
+ console.log(X+Y); 
+}
